10953604da4717ffde573d503a8ae0ab10620edf
Yans .R Hidalgo <yans.rollhidalgo@bloomsisters.swiss>
2020-08-07 17:53:50 +0200

    updated PS script!

--- CustomScripts/BuildWVD.ps1
+++ CustomScripts/BuildWVD.ps1
@@ -1,5 +1,7 @@
 # Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+# Partition & Format the data disk
+Get-Disk | Where-Object PartitionStyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "disk2" -Confirm:$false
 # Install Docker Desktop
 choco install docker-desktop -y
 # Restart Computer
