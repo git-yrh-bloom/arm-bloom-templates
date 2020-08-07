649417ed29a1b6a7dd2eba43026e0d470ffe42c1
Yans .R Hidalgo <yans.rollhidalgo@bloomsisters.swiss>
2020-08-07 17:34:42 +0200

    updated PS script!

--- CustomScripts/BuildWVD.ps1
+++ CustomScripts/BuildWVD.ps1
@@ -1,3 +1,6 @@
+# Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
+# Install Docker Desktop
 choco install docker-desktop -y
+# Restart Computer
+Restart-Computer -Force
