17e03b69a7f51c3dd9ffaee359a913d380d21ee4
Yans .R Hidalgo <yans.rollhidalgo@bloomsisters.swiss>
2020-08-29 11:58:46 +0200

    Updated Script!

--- CustomScripts/BuildWVD.ps1
+++ CustomScripts/BuildWVD.ps1
@@ -1,7 +1,15 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 # Partition & Format the data disk
-Get-Disk | Where-Object PartitionStyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "disk2" -Confirm:$false
+Get-Disk | Where-Object PartitionStyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "DataDisk1" -Confirm:$false
 # Install Docker Desktop
-choco install docker-desktop -y
+# choco install docker-desktop -y
+# Install PowerShell-Core 7
+choco install powershell-core -y
+# Instal Azure Data Studio
+choco install azure-data-studio -y
+# Scan for updates and install them, including other microsoft products
+$Updates = Start-WUScan -SearchCriteria "IsInstalled=0 AND IsHidden=0 AND IsAssigned=1"
+Write-Host "Updates found: " $Updates.Count
+Install-WUUpdates -Updates $Updates
 # Restart Computer
 Restart-Computer -Force
