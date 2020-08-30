50bdb7cd636d1691615dea2c8d002c9c1a65adf6
Yans .R Hidalgo <yans.rollhidalgo@bloomsisters.swiss>
2020-08-30 08:46:16 +0200

    Remove MS Built-in Apps

--- CustomScripts/BuildWVD.ps1
+++ CustomScripts/BuildWVD.ps1
@@ -1,6 +1,17 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 # Partition & Format the data disk
 Get-Disk | Where-Object PartitionStyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "DataDisk1" -Confirm:$false
+# Remove MS Built-in Apps
+Get-AppxPackage *windowscamera* | Remove-AppxPackage
+Get-AppxPackage *officehub* | Remove-AppxPackage
+Get-AppxPackage *skypeapp* | Remove-AppxPackage
+Get-AppxPackage *zunemusic* | Remove-AppxPackage
+Get-AppxPackage *windowsmaps* | Remove-AppxPackage
+Get-AppxPackage *solitairecollection* | Remove-AppxPackage
+Get-AppxPackage *zunevideo* | Remove-AppxPackage
+Get-AppxPackage *onenote* | Remove-AppxPackage
+Get-AppxPackage *windowsphone* | Remove-AppxPackage
+Get-AppxPackage *xboxapp* | Remove-AppxPackage
 # Install Microsoft Edge
 choco install microsoft-edge -y
 # Install Docker Desktop
